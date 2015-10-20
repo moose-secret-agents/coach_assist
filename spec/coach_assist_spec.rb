8a008c2bf8f1fe6da5e40ff2ef1ae4be66a94ff0
Urs Gerber <ug.gerber@gmail.com>
2015-10-20 16:42:31 +0200

    Fix test

--- spec/coach_assist_spec.rb
+++ spec/coach_assist_spec.rb
@@ -4,8 +4,4 @@ describe CoachAssist do
   it 'has a version number' do
     expect(CoachAssist::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
